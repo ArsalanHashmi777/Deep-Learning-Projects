8aabe900009a07b6af55264f5f5b881ba4a6636c
Arsalan <arsalanhashe@gmail.com>
2026-05-03 22:22:51 +0500

    Rewrite layers.py file.

--- Neural-Network-Scratch/src/layers.py
+++ Neural-Network-Scratch/src/layers.py
@@ -1,32 +1,40 @@
 import numpy as np
 
-def initialize_parameters(layer_dims):
+def init_params(layer_dims):
     """
-    Arguments:
-    layer_dims -- python array (list) containing the dimensions of each layer in our network
-    
-    Returns:
-    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
-                    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
-                    bl -- bias vector of shape (layer_dims[l], 1)
+    Initializes weights and biases for each layer.
+    Input: layer_dims -- list containing the dimensions of each layer [784, 64, 32, 10]
     """
-    np.random.seed(1) # For consistency
-    parameters = {}
-    L = len(layer_dims)            
+    params = {}
+    L = len(layer_dims) # Number of layers in the network
 
     for l in range(1, L):
-        # He Initialization (Professional standard for ReLU layers)
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * np.sqrt(2/layer_dims[l-1])
-        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        # He Initialization: Good for ReLU activations to keep gradients stable
+        params['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * np.sqrt(2 / layer_dims[l-1])
+        params['b' + str(l)] = np.zeros((layer_dims[l], 1))[cite: 3]
         
-    return parameters
+    return params
 
 def linear_forward(A_prev, W, b):
     """
-    Implement the linear part of a layer's forward propagation.
-    Formula: Z = W * A_prev + b
+    Computes the linear part of a layer's forward propagation.
+    Z = W.A_prev + b
     """
-    Z = np.dot(W, A_prev) + b
-    cache = (A_prev, W, b) # Stored for backpropagation
-    
+    Z = np.dot(W, A_prev) + b[cite: 3]
+    # We save A_prev, W, and b in a cache because we need them for backprop later[cite: 3]
+    cache = (A_prev, W, b)
     return Z, cache
+
+def linear_backward(dZ, cache):
+    """
+    Computes the gradients dW, db, and dA_prev using the chain rule.[cite: 3]
+    """
+    A_prev, W, b = cache
+    m = A_prev.shape[1] # Number of examples
+
+    # Math: The gradients of our weights and biases[cite: 3]
+    dW = (1 / m) * np.dot(dZ, A_prev.T)
+    db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(W.T, dZ) # This "passes" the error back to the previous layer[cite: 3]
+
+    return dA_prev, dW, db
